dab98dc4177d054944ef87c57b8553fdd3341f47
spde3289 <spde3289@naver.com>
2025-11-06 14:53:44 +0900

    fix(api): 아이돌 차트 요청 api 수정

--- src/api/chartClient.js
+++ src/api/chartClient.js
@@ -1,7 +1,7 @@
 import { Get, Put } from "./client";
 import PATCHS from "./path";
 
-export const getChartList = async (config) => {
-  const response = await Get(PATCHS.charts, config);
+export const getChartList = async (gender, config) => {
+  const response = await Get(`${PATCHS.charts}/${gender}`, config);
   return response;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,14 @@
+import { getChartList } from "@/api/chartClient";
+import { useEffect } from "react";
+
 function Home() {
+  useEffect(() => {
+    getChartList("female", {
+      params: { page: 1, size: 10 },
+    }).then((response) => {
+      console.log(response);
+    });
+  }, []);
   return (
     <>
       <div>나는 홈페이지임</div>
